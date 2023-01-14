d8e3cb3d8c27a68ebfeed6ed73c4f11d79a2a217
wahidari <wahiid.ari@gmail.com>
2023-01-14 14:50:32 +0700

    change theme color and logo, add changelog, release, roadmap

--- theme.config.js
+++ theme.config.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import logo from "@public/logo.png";
+import Image from "next/image";
 
 const Logo = ({ height }) => (
-  <svg height={height} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-  </svg>
+  <Image src="/icon.png" alt="Icon" className="rounded-full" width={100} height={100} />
 )
 
 const config = {
+  primaryHue: 160,
   logo: () => {
     const { route } = useRouter()
     return (
       <>
-        <Logo height={18} />
+        {/* <Logo height={18} /> */}
+        <Image src="/icon.jpg" alt="Icon" className="rounded-full" width={24} height={24} />
         {/* {route === '/' ? null : ( */}
         <span
           className='mx-2 font-bold select-none'
