afa7ca920681eeb99d82acb25517e5d5e77e86db
wahidari <wahiid.ari@gmail.com>
2023-01-14 18:05:54 +0700

    add API Playground

--- components/ApiPlayground.js
+++ components/ApiPlayground.js
@@ -106,7 +106,7 @@ export default function ApiPlayground({ method, endpoint, children, param = fals
         `${methodUpperCase == "DELETE" ? "bg-red-600 hover:bg-red-700" : ""}`,
       )}>
         <svg className="fill-white h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z"></path></svg>
-        {!loading ? "Send Request" : "Sending..."}
+        {!loading ? "Send Request" : "Sending Request..."}
       </button>
 
       {fetched ?
