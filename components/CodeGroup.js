cebcd0fe686e47d1fb3305a80e35c26fbf3d5f9d
wahidari <wahiid.ari@gmail.com>
2023-01-14 23:24:18 +0700

    fix CodeGroup component overflow

--- components/CodeGroup.js
+++ components/CodeGroup.js
@@ -6,13 +6,12 @@ import { AnimateSharedLayout, motion } from "framer-motion";
 export default function CodeGroup({ items, children }) {
   return (
     <Tab.Group as="div" className="req-res my-4">
-      <Tab.List className="flex items-center relative bg-blue-100/70 dark:bg-neutral-800/75 rounded-t-xl overflow-auto">
+      <Tab.List className="flex items-center relative bg-blue-100/70 dark:bg-neutral-800/75 rounded-t-xl overflow-auto" >
         <AnimateSharedLayout>
           {items.map((item, index) => (
             <Tab key={index + 1} as="div" className="group rounded">
               {({ selected }) => (
                 <button className={clsx("relative px-4 py-2 text-[13px] font-medium transition-all duration-200",
-                  "truncate",
                   selected ? "text-neutral-800 dark:text-neutral-100" : "text-neutral-600 hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100",
                 )}>
                   {item}
