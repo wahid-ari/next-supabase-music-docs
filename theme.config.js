833be06092864435dd33fbb42fcea4898e23154d
wahidari <wahiid.ari@gmail.com>
2023-02-19 16:36:10 +0700

    add OG Image Generator

--- theme.config.js
+++ theme.config.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 import Image from "next/image";
 
 const Logo = ({ height }) => (
@@ -44,34 +45,43 @@ const config = {
       }
     }
   },
-  head: (
-    <>
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  head: function useHead() {
+    const { title } = useConfig()
+    const { route } = useRouter()
+    const socialCard =
+      route === '/' || !title
+        ? 'https://my-music-docs.vercel.app/og.png'
+        : `https://my-music-docs.vercel.app/api/og?title=${title}`
 
-      <meta name="description" content="MyMusic Docs" />
-      <meta name="og:description" content="MyMusic Docs" />
-      <meta name="og:title" content="MyMusic" />
-      <meta name="og:image" content="https://my-music-docs.vercel.app/og.png" />
+    return (
+      <>
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site:domain" content="my-music-docs.vercel.app" />
-      <meta name="twitter:site" content="my-music-docs.vercel.app" />
-      <meta name="twitter:url" content="https://my-music-docs.vercel.app" />
-      <meta name="twitter:image" content="https://my-music-docs.vercel.app/og.png" />
+        <meta name="description" content="MyMusic Docs" />
+        <meta name="og:description" content="MyMusic Docs" />
+        <meta name="og:title" content="MyMusic" />
+        <meta name="og:image" content={socialCard} />
 
-      <meta name="apple-mobile-web-app-title" content="MyMusic" />
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-    </>
-  ),
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site:domain" content="my-music-docs.vercel.app" />
+        <meta name="twitter:site" content="my-music-docs.vercel.app" />
+        <meta name="twitter:url" content="https://my-music-docs.vercel.app" />
+        <meta name="twitter:image" content={socialCard} />
+
+        <meta name="apple-mobile-web-app-title" content="MyMusic" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      </>
+    )
+  },
   docsRepositoryBase: 'https://github.com/wahid-ari/next-supabase-music-docs/tree/master/',
   banner: {
     key: 'banner',
